29f6238a0eefb7a9468bda8a431d66a15711c315
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-06-13 20:39:22 -0700

    Creating & Appending Elements Assignment

---
+++ week2/BasicWebsit.js/BasicSite.js
@@ -0,0 +1,8 @@
+var newBaby = document.body
+var h1 = document.createElement("h1")
+h1.textContent = "hello world"
+newBaby.append(h1)
+var nav = document.createElement("nav")
+nav.textContent = "About"
+nav.append(nav)
+
---
+++ week2/Creating&AppendingElements/Creating&AppendingElements.js
@@ -0,0 +1,24 @@
+var newLi = document.createElement("h1")
+newLi.textContent = "Welcome to my JS site"
+var  newP = document.getElementById("point")
+newP.append(newLi)
+var newG = document.createElement("p") 
+newG.textContent = "All of this was created with Javascript"
+var newT = document.getElementById("point") 
+newT.append(newG)
+var listR = document.createElement("ol")
+var listU = document.getElementById("point")
+listU.append(listR)
+var olList = document.createElement("li")
+olList.textContent = "I can see"
+listR.append(olList)
+var listT = document.getElementById("point")
+var listOl = document.createElement("li")
+listOl.textContent = "Clearly now the"
+listR.append(listOl)
+var listS = document.getElementById("point")
+var listAl = document.createElement("li")
+listAl.textContent = "Rain is gone"
+listR.append(listAl)
+var listH = document.getElementById("li")
+("point")
