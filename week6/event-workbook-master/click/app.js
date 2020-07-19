3b150f21d8fd6179a6920c43c35c42d1c08ed5b9
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-07-19 12:36:00 -0700

    Capstone Project Assignment 2

--- week6/event-workbook-master/click/app.js
+++
@@ -1,7 +0,0 @@
-// make the box disapear when the user clicks it
-var redbox =  document.getElementsByClassName("red-box")[0]
-console.log(redbox)
-redbox.addEventListener("click", function(){
-    redbox.remove()
-}
-)
--- week6/event-workbook-master/mousemove/app.js
+++
@@ -1,8 +0,0 @@
-
-var output = document.getElementsByClassName("red-box")[0]
- 
-output.addEventListener("mousemove", function(event){   
-    output.textContent = event.x + " " + event.y
-    console.log(event)
-   
-})
